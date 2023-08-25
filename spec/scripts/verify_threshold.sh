dbd15c45ef77e60c74fa666556deff5b22593110
wdower <will@dower.dev>
2023-08-25 11:22:02 -0400

    correctly structured envars in verify_threshold, removing debug statements
    Signed-off-by: wdower <will@dower.dev>

--- spec/scripts/verify_threshold.sh
+++ spec/scripts/verify_threshold.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 set -uo pipefail
 
-pwd
-echo "ls'ing the main repo" 
-ls -lah
-
-echo "ls'ing reports"
-ls -lah reports
-
 # catch misnamed Trivy report
 if [ -f "reports/aquasecurity - aquasecurity.json" ]; then
     mv "reports/aquasecurity - aquasecurity.json" "reports/Aqua Security - Trivy.json"
 fi
 
-INSPEC_THRESHOLD_CHECK=$(saf validate threshold -F inspec.threshold.yml -i "$REPORT_DIR/Aqua Security - Trivy.json")
-TRIVY_THRESHOLD_CHECK=$(saf validate threshold -F trivy.threshold.yml -i "$REPORT_DIR/inspec_results.json")
+saf validate threshold -F inspec.threshold.yml -i "$REPORT_DIR/Aqua Security - Trivy.json"
+INSPEC_THRESHOLD_CHECK=$?
+saf validate threshold -F trivy.threshold.yml -i "$REPORT_DIR/inspec_results.json"
+TRIVY_THRESHOLD_CHECK=$?
 
 if  [ $INSPEC_THRESHOLD_CHECK -eq 0 ] && [ $TRIVY_THRESHOLD_CHECK -eq 0 ]; then
     docker tag $TARGET_IMAGE:testing $TARGET_IMAGE:passed
