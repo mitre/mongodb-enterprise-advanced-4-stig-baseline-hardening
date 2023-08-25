05e3e7f700dc7c49196701b881d3f2dc287183cd
wdower <will@dower.dev>
2023-08-25 10:33:41 -0400

    trying to fix verify_threshold if statement, using the checkout action instead of hardcoding it in the workfow file
    Signed-off-by: wdower <will@dower.dev>

--- spec/scripts/verify_threshold.sh
+++ spec/scripts/verify_threshold.sh
@@ -7,8 +7,7 @@ ls -lah
 INSPEC_THRESHOLD_CHECK=$(saf validate threshold -F inspec.threshold.yml -i "$REPORT_DIR/Aqua Security - Trivy.json")
 TRIVY_THRESHOLD_CHECK=$(saf validate threshold -F trivy.threshold.yml -i "$REPORT_DIR/inspec_results.json")
 
-if  [ $INSPEC_THRESHOLD_CHECK -eq 0 ] &&
-    [ $TRIVY_THRESHOLD_CHECK -eq 0 ]; then
+if  [ $INSPEC_THRESHOLD_CHECK -eq 0 ] && [ $TRIVY_THRESHOLD_CHECK -eq 0 ]; then
     docker tag $TARGET_IMAGE:testing $TARGET_IMAGE:passed
     docker image rm $TARGET_IMAGE:testing
     exit 0
